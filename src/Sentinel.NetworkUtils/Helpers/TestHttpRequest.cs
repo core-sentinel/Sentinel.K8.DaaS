3fe49a520571dab0d562b1184f9578e485df641b
Murat MERCAN <mmercan@outlook.com>
2024-05-14 12:51:31 +1000

    http req

--- src/Sentinel.NetworkUtils/Helpers/TestHttpRequest.cs
+++ src/Sentinel.NetworkUtils/Helpers/TestHttpRequest.cs
@@ -17,7 +17,6 @@ public static class TestHttpRequest
             var response = await client.SendAsync(req);
             string message = response.StatusCode.ToString() + " " + await response.Content.ReadAsStringAsync();
 
-
             return new TestNetConnectionResponse(CheckAccessRequestResourceType.Http, true, message, sw.ElapsedMilliseconds);
         }
         catch (Exception ex)
